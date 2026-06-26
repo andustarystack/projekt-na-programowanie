e0be1f1a1734f7d9835d7671c671c377d6f1676a
andustarystack <andu.stary@gmail.com>
2026-06-26 14:39:28 +0200

    Create zadanie_9_cleanup.sh

---
+++ zadania/zadanie_9_cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd kopie/ || exit
+for plik in *.zip; do
+    rok=$(echo "$plik" | cut -c 1-4)
+    miesiac=$(echo "$plik" | cut -c 6-7)
+    mkdir -p "$rok/$miesiac"
+    mv "$plik" "$rok/$miesiac/"
+done
+cd ..
